f7bcbc73e658d01933fc3502e4f7c46fb0d810bf
muhdusama <46465291+muhdusama@users.noreply.github.com>
2026-08-12 14:31:10 +1000

    Install runtime security guards

--- radar/__init__.py
+++ radar/__init__.py
@@ -1,1 +1,7 @@
 """Software Release Radar dashboard."""
+
+from .runtime_security import install_runtime_guards as _install_runtime_guards
+
+_install_runtime_guards()
+
+del _install_runtime_guards
